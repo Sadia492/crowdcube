d473d2f8dc41b36b388e0e5e33b87c8270158cb0
Sadia492 <sadiaafrin2792>
2024-12-04 22:05:36 +0600

    toggle functionality added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import RunningCampaign from "../components/RunningCampaign";
 import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
   const runningCampaigns = useLoaderData();
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
+
+  const themeToggle = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
 
   return (
     <div>
+      <button onClick={themeToggle} className="btn btn-outline">
+        Toggle to {theme === "light" ? "Dark" : "Light"} Theme
+      </button>
       <Banner></Banner>
       <RunningCampaign runningCampaigns={runningCampaigns}></RunningCampaign>
     </div>
